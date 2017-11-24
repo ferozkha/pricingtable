af2170f4851ac6ab97c78ac54512fb32b7ee01e8
ferozkha <32040637+ferozkha@users.noreply.github.com>
2017-11-24 15:59:54 +0600

    frist time try paicing table

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+//$(document).ready(function(){
+//    $(".single_table").hover(function(){
+//        $(".single_table").removeClass("active")
+//        $(this).addClass("active");
+//    })
+//    
+//})
+$(document).ready(function(){
+  $(".single_table").hover(function(){
+      $(".single_table").removeClass("active");
+      $(this).addClass("active");
+  }) 
+    
+});
+
